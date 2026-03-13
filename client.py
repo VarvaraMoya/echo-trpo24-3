dd5f3f1a653d334994c0fc957551840c16e9dd61
VarvaraMoya <maslovavarvara08@gmail.com>
2026-03-13 11:11:54 +0300

    worked version

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('localhost', 9090))
 
-#msg = input()
+msg = input("Input text: ")
 msg = "Hi!"
 sock.send(msg.encode())
 
